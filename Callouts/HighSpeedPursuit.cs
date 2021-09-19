2a81b8308b867e6906b3e0a16252002955f79c66
AchtungScaar <90028069+AshtonTheDeveloper@users.noreply.github.com>
2021-09-19 19:16:18 +0100

    Added first callout & fished setup

---
+++ Callouts/HighSpeedPursuit.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using System.Drawing;
+
+namespace LSCallouts.Callouts
+{
+    [CalloutInfo("High Speed Pursuit", CalloutProbability.Medium)]
+    public class HighSpeedPursuit : Callout
+    {
+        private Ped Suspect;
+        private Vehicle SuspectVehicle;
+        private Blip SuspectBlip;
+        private LHandle Pursuit;
+        private Vector3 Spawnpoint;
+        private bool PursuitCreated;
+
+        public override bool OnBeforeCalloutDisplayed()
+        {
+            Spawnpoint = World.GetRandomPositionOnStreet();
+            ShowCalloutAreaBlipBeforeAccepting(Spawnpoint, 40f);
+            AddMinimumDistanceCheck(40f, Spawnpoint);
+            CalloutMessage = "High Speed Pursuit On The Streets";
+            CalloutPosition = Spawnpoint;
+            Functions.PlayScannerAudioUsingPosition("WE_HAVE CRIME_RESISTING_ARREST_02 IN_OR_ON_POSITION", Spawnpoint);
+
+            return base.OnBeforeCalloutDisplayed();
+        }
+
+        public override bool OnCalloutAccepted()
+        {
+            // Suspect Vehicle Configuration
+            SuspectVehicle = new Vehicle("BULLET", Spawnpoint);
+            SuspectVehicle.IsPersistent = true;
+
+            // The Suspect Configuration
+            Suspect = new Ped(SuspectVehicle.GetOffsetPositionFront(8f));
+            Suspect.IsPersistent = true;
+            Suspect.BlockPermanentEvents = true;
+            Suspect.WarpIntoVehicle(SuspectVehicle, -1);
+
+            // Suspect Blip Configuration
+            SuspectBlip = Suspect.AttachBlip();
+            SuspectBlip.Color = System.Drawing.Color.Red;
+            SuspectBlip.IsRouteEnabled = true;
+
+            // Is The Pursuit Created?
+            PursuitCreated = false;
+
+            return base.OnCalloutAccepted();
+        }
+
+        public override void Process()
+        {
+            base.Process();
+
+            if (!PursuitCreated && Game.LocalPlayer.Character.DistanceTo(SuspectVehicle) <= 30f)
+            {
+                Pursuit = Functions.CreatePursuit();
+                Functions.AddPedToPursuit(Pursuit, Suspect);
+                Functions.SetPursuitIsActiveForPlayer(Pursuit, true);
+                PursuitCreated = true;
+            }
+
+            if (PursuitCreated && !Functions.IsPursuitStillRunning(Pursuit))
+            {
+                End();
+            }
+        }
+        public override void End()
+        {
+            base.End();
+
+            // Cleanup
+            if (Suspect.Exists())
+            {
+                Suspect.Dismiss();
+            }
+            if (SuspectBlip.Exists())
+            {
+                SuspectBlip.Delete();
+            }
+            if (SuspectVehicle.Exists())
+            {
+                SuspectVehicle.Dismiss();
+            }
+
+            Game.LogTrivial("LSCallouts Information: HSP1 Cleaned up and code 4");
+
+        }
+    }
+}
--- Main.cs
+++ Main.cs
@@ -3,10 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using Rage;
+using LSPD_First_Response.Mod.API;
 
 namespace LSCallouts
 {
-    public class Main
+    public class Main: Plugin
     {
+        public override void Initialize()
+        {
+            Functions.OnOnDutyStateChanged += OnOnDutyStateChangedHandler;
+            Game.LogTrivial("LSCallouts" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " developed by Code99 has been initialised.");
+            Game.LogTrivial("LSCallouts Information: LSCallouts will be fully loaded when you have gone on duty.");
+
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(LSPDFRResolveEventHandler);
+        }
+
+        public override void Finally()
+        {
+            Game.LogTrivial("LSCallouts Information: LSCallouts has been cleaned up.");
+        }
+
+        private static void OnOnDutyStateChangedHandler(bool OnDuty)
+        {
+            if (OnDuty)
+            {
+                RegisterLSCallouts();
+                Game.DisplayNotification("LSCallouts | Version A1.0.0 | Alpha - Loaded");
+            }
+        }
+
+        private static void RegisterLSCallouts()
+        {
+            // High Speed Pursuit Callouts
+            Functions.RegisterCallout(typeof(Callouts.HighSpeedPursuit));
+        }
+
+        public static Assembly LSPDFRResolveEventHandler(object sender, ResolveEventArgs args)
+        {
+            foreach (Assembly assembly in Functions.GetAllUserPlugins())
+            {
+                if (args.Name.ToLower().Contains(assembly.GetName().Name.ToLower()))
+                {
+                    return assembly;
+                }
+            }
+            return null;
+        }
+        public static bool IsLSPDFRPluginRunning(string Plugin, Version minversion = null)
+        {
+            foreach (Assembly assembly in Functions.GetAllUserPlugins())
+            {
+                AssemblyName an = assembly.GetName();
+                if (an.Name.ToLower() == Plugin.ToLower())
+                {
+                    if (minversion == null || an.Version.CompareTo(minversion) >= 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
